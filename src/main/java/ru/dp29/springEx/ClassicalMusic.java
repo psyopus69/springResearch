b45f2f20ebf92d715c5dfd7dd1b26708d7ba2e58
psyopus69 <GitBest666>
2021-01-06 01:39:00 +0300

    add init-method destroy-method factory-method

--- src/main/java/ru/dp29/springEx/ClassicalMusic.java
+++ src/main/java/ru/dp29/springEx/ClassicalMusic.java
@@ -1,6 +1,19 @@
 package ru.dp29.springEx;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     public String getSong() {
         return "Biggie Smalls";
     }
--- src/main/java/ru/dp29/springEx/TestSpring.java
+++ src/main/java/ru/dp29/springEx/TestSpring.java
@@ -8,23 +8,28 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("classicMusicBean", ClassicalMusic.class);
+
+//        ClassicalMusic cm = ClassicalMusic.getClassicalMusic();
+//        System.out.println(cm.getSong());
+        System.out.println(classicalMusic.getSong());
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println("comparison: " + comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println("comparison: " + comparison);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        firstMusicPlayer.setVolume(111);
+//        firstMusicPlayer.setVolume(111);
 //        firstMusicPlayer.playMusic();
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
