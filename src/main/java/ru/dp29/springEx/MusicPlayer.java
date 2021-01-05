49f60bb8dee4f56e581f5c1a899dfda0946e4da2
psyopus69 <GitBest666>
2021-01-06 00:43:31 +0300

    add collections of bean's

--- src/main/java/ru/dp29/springEx/MusicPlayer.java
+++ src/main/java/ru/dp29/springEx/MusicPlayer.java
@@ -1,13 +1,33 @@
 package ru.dp29.springEx;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    // IoC
+    public MusicPlayer() {}
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
