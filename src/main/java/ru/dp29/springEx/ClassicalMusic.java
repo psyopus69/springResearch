840899a5b1f7d566d2aa1f2726e9f3a705b81cc4
psyopus69 <GitBest666>
2021-01-07 00:40:23 +0300

    add annotations @Value @Scope @PostConstruct @PreDestroy

--- src/main/java/ru/dp29/springEx/ClassicalMusic.java
+++ src/main/java/ru/dp29/springEx/ClassicalMusic.java
@@ -4,6 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("doing my initialization");
+    }
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("doing my destroy");
+    }
     @Override
     public String getSong() {
         return "Biggie Smalls";
